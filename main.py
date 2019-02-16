07a3ba99e6b2773f458c927278dc3aefb7ac0fd2
johnpoint <me@lvcshu.info>
2019-02-16 16:26:51 +0800

    update

--- main.py
+++ main.py
@@ -28,21 +28,21 @@ def welcome_new(message):
             try:
                 userfirstname = message.new_chat_members[0].first_name
                 userlastname = message.new_chat_members[0].last_name
-                msg1 = bot.send_message(
-                    message.chat.id, '%s %s 欢迎加入津津乐道听友的大家庭~\n在这里你可以尽情的与主播以及其他听友进行交流，但是要注意不要发广告哦！\nhttps://t.me/htnpodcast/44627' % (userfirstname,userlastname)).message_id
-                msg2 = bot.send_message(
-                    message.chat.id, '很高兴认识你，我是群内的小助手，点击\n--> /help <--\n试试吧').message_id
+                msg1 = bot.reply_to(
+                    message, '%s %s 欢迎加入津津乐道听友的大家庭~\n在这里你可以尽情的与主播以及其他听友进行交流，但是要注意不要发广告哦！\nhttps://t.me/htnpodcast/44627' % (userfirstname,userlastname)).message_id
+                msg2 = bot.reply_to(
+                    message, '很高兴认识你，我是群内的小助手，点击\n--> /help <--\n试试吧').message_id
             except AttributeError:
-                msg1 = bot.send_message(
-                    message.chat.id, '欢迎加入津津乐道听友的大家庭~\n在这里你可以尽情的与主播以及其他听友进行交流，但是要注意不要发广告哦！\nhttps://t.me/htnpodcast/44627').message_id
-                msg2 = bot.send_message(
-                    message.chat.id, '很高兴认识你，我是群内的小助手，点击\n--> /help <--\n试试吧').message_id
+                msg1 = bot.reply_to(
+                    message, '欢迎加入津津乐道听友的大家庭~\n在这里你可以尽情的与主播以及其他听友进行交流，但是要注意不要发广告哦！\nhttps://t.me/htnpodcast/44627').message_id
+                msg2 = bot.reply_to(
+                    message, '很高兴认识你，我是群内的小助手，点击\n--> /help <--\n试试吧').message_id
         else:
             msg1 = bot.send_message(
                 message.chat.id, '@%s 欢迎加入津津乐道听友的大家庭~\n在这里你可以尽情的与主播以及其他听友进行交流，但是要注意不要发广告哦！\nhttps://t.me/htnpodcast/44627' % username).message_id
             msg2 = bot.send_message(
                 message.chat.id, '很高兴认识你，我是群内的小助手，点击\n--> /help <--\n试试吧').message_id
-        bot.delete_message(message.chat.id, message.message_id)
+            bot.delete_message(message.chat.id, message.message_id)
         time.sleep(20)
         bot.delete_message(message.chat.id, msg1)
         bot.delete_message(message.chat.id, msg2)
