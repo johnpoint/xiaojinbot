7914cb66e80d6d89eec8a0a810ae7d7cf92349f9
johnpoint <me@lvcshu.com>
2019-03-20 10:43:41 +0800

    update

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def welcome_new(message):
 
 @bot.message_handler(commands=['verify'])
 def send_verify(message):
-    print('[Info] send reply for /verify')
+    print('[Info] send reply for /verify')  
     msg = bot.send_message(message.chat.id, '验证成功!限制已经解除').message_id
     bot.delete_message(message.chat.id, message.message_id)
     time.sleep(10)
